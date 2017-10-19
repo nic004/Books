a7f15cc7aaf26265705fd80055cac7a7ee8881cd
nathan.choi <nathan.choi@oz1.com>
2017-10-19 11:42:20 +0900

    스크립트 추가

--- server/controllers/paragraphs.js
+++ server/controllers/paragraphs.js
@@ -15,10 +15,26 @@ router.get('/', (req, res) => {
   });
 });
 
+function naturalCompare(a, b) {
+    var ax = [], bx = [];
+
+    a.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { ax.push([$1 || Infinity, $2 || ""]) });
+    b.replace(/(\d+)|(\D+)/g, function(_, $1, $2) { bx.push([$1 || Infinity, $2 || ""]) });
+    
+    while(ax.length && bx.length) {
+        var an = ax.shift();
+        var bn = bx.shift();
+        var nn = (an[0] - bn[0]) || an[1].localeCompare(bn[1]);
+        if(nn) return nn;
+    }
+
+    return ax.length - bx.length;
+}
+
 router.get('/captures', (req, res) => {
   const dir = path.resolve(__dirname + '/../../static/images/captures/');
   fs.readdir(dir, (err, files) => {
-    const pngs = files.filter((file) => /.+\.(png|jpg|jpeg)/i.test(file)).map((img) => `/static/images/captures/${img}`);
+    const pngs = files.filter((file) => /.+\.(png|jpg|jpeg)/i.test(file)).map((img) => `/static/images/captures/${img}`).sort(naturalCompare);
     res.json({urls: pngs});
   });
 });
